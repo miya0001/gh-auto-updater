5748d8ce6f1ebe439a3628126812484ce70871d4
miya0001 <miya@wpist.me>
2017-02-05 06:45:38 +0900

    add tests for some function

---
+++ tests/test-gh-auto-updater.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Gh_Auto_Updater_Example
+ */
+
+use Miya\WP\GH_Auto_Update;
+
+/**
+ * Sample test case.
+ */
+class GH_Auto_Update_Test extends WP_UnitTestCase
+{
+	protected $updater;
+
+	/**
+	 * get_gh_api()
+	 */
+	function test_get_gh_api()
+	{
+		$gh_user = 'miya0001';
+		$gh_repo = 'gh-auto-updater-example';
+		$updater = new GH_Auto_Update( $gh_user, $gh_repo, __FILE__ );
+
+		// For a private function.
+		$reflection = new \ReflectionClass( $updater );
+		$method = $reflection->getMethod( 'get_gh_api' );
+		$method->setAccessible( true );
+		$res = $method->invoke( $updater );
+		$this->assertSame(
+			"https://api.github.com/repos/miya0001/gh-auto-updater-example/releases/latest",
+			$res
+		);
+	}
+
+	/**
+	 * get_api_data()
+	 */
+	function test_get_api_data()
+	{
+		$gh_user = 'miya0001';
+		$gh_repo = 'gh-auto-updater-example';
+		$updater = new GH_Auto_Update( $gh_user, $gh_repo, __FILE__ );
+
+		// For a private function.
+		$reflection = new \ReflectionClass( $updater );
+		$method = $reflection->getMethod( 'get_api_data' );
+		$method->setAccessible( true );
+		$res = $method->invoke( $updater );
+		$expect = "https://api.github.com/repos/miya0001/gh-auto-updater-example/releases";
+		$this->assertTrue( 0 === strpos( $res->url, $expect ) );
+		$this->assertSame( 1, count( $res->assets ) );
+	}
+}
