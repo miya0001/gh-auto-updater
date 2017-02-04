29e9cd76e32bfbc59c48f80d557eda3761ee0e3d
miya0001 <miya@wpist.me>
2017-02-04 18:35:47 +0900

    fix namespace problem

--- src/GH_Auto_Update.php
+++ src/GH_Auto_Update.php
@@ -42,7 +42,7 @@ class GH_Auto_Update
 		$current_version = $this->get_plugin_info();
 
 		if ( version_compare( $current_version['Version'], $remote_version->tag_name, '<' ) ) {
-			$obj = new stdClass();
+			$obj = new \stdClass();
 			$obj->slug = $this->slug;
 			$obj->new_version = $remote_version->tag_name;
 			$obj->url = $remote_version->html_url;
@@ -60,7 +60,7 @@ class GH_Auto_Update
 			$remote_version = $this->get_api_data();
 			$current_version = $this->get_plugin_info();
 
-			$obj = new stdClass();
+			$obj = new \stdClass();
 			$obj->slug = $this->slug;
 			$obj->name = $current_version['Name'];
 			$obj->new_version = $remote_version->tag_name;
