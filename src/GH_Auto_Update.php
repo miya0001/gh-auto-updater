3437881d7401fd8c1d4dc13ca83a4c71c660ee48
miya0001 <miya@wpist.me>
2017-02-05 19:31:24 +0900

    fixed #2

--- src/GH_Auto_Update.php
+++ src/GH_Auto_Update.php
@@ -6,15 +6,13 @@ class GH_Auto_Update
 {
 	private $gh_user;
 	private $gh_repo;
-	private $plugin_file;
 	private $slug;
 
-	public function __construct( $plugin_file, $gh_user, $gh_repo )
+	public function __construct( $plugin_slug, $gh_user, $gh_repo )
 	{
 		$this->gh_user     = $gh_user;
 		$this->gh_repo     = $gh_repo;
-		$this->plugin_file = $plugin_file;
-		$this->slug        = plugin_basename( $plugin_file );
+		$this->slug        = $plugin_slug;
 
 		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'pre_set_site_transient_update_plugins' ) );
 		add_filter( 'plugins_api', array( $this, 'plugins_api' ), 10, 3 );
@@ -40,18 +38,13 @@ class GH_Auto_Update
 		}
 
 		$remote_version = $this->get_api_data();
+		if ( is_wp_error( $remote_version ) ) {
+			return $transient;
+		}
+
 		$current_version = $this->get_plugin_info();
 
-		if ( version_compare( $current_version['Version'], $remote_version->tag_name, '<' ) ) {
-			$obj = new \stdClass();
-			$obj->slug = $this->slug;
-			$obj->new_version = $remote_version->tag_name;
-			$obj->url = $remote_version->html_url;
-			$obj->plugin = $this->slug;
-			$obj->package = $this->get_download_url( $remote_version );
-			$transient->response[ $this->slug ] = $obj;
-		}
-		return $transient;
+		return $this->get_newer_version( $transient, $current_version, $remote_version );
 	}
 
 	public function plugins_api( $obj, $action, $arg )
@@ -59,6 +52,9 @@ class GH_Auto_Update
 		if ( ( 'query_plugins' === $action || 'plugin_information' === $action ) &&
 				isset( $arg->slug ) && $arg->slug === $this->slug ) {
 			$remote_version = $this->get_api_data();
+			if ( is_wp_error( $remote_version ) ) {
+				return $obj;
+			}
 			$current_version = $this->get_plugin_info();
 
 			$obj = new \stdClass();
@@ -76,40 +72,61 @@ class GH_Auto_Update
 		return $obj;
 	}
 
+	private function get_newer_version( $transient, $current_version, $remote_version )
+	{
+		if ( version_compare( $current_version['Version'], $remote_version->tag_name, '<' ) ) {
+			$obj = new \stdClass();
+			$obj->slug = dirname( $this->slug );
+			$obj->plugin = $this->slug;
+			$obj->new_version = $remote_version->tag_name;
+			$obj->url = $remote_version->html_url;
+			$obj->package = $this->get_download_url( $remote_version );
+			$transient->response[ $this->slug ] = $obj;
+		}
+
+		return $transient;
+	}
+
 	private function get_download_url( $remote_version )
 	{
 		if ( ! empty( $remote_version->assets[0] )
 				&& ! empty( $remote_version->assets[0]->browser_download_url ) ) {
-			return $remote_version->assets[0]->browser_download_url;
+			$download_url = $remote_version->assets[0]->browser_download_url;
 		} else {
-			return sprintf(
+			$download_url = sprintf(
 				'https://github.com/%s/%s/archive/%s.zip',
 				$this->gh_user,
 				$this->gh_repo,
 				$remote_version->tag_name
 			);
 		}
+
+		return esc_url( $download_url );
 	}
 
 	private function get_plugin_info()
 	{
-		$plugin = get_plugin_data( $this->plugin_file );
+		$plugin = get_plugin_data( WP_PLUGIN_DIR . '/' . $this->plugin_slug );
 		return $plugin;
 	}
 
 	private function get_api_data()
 	{
 		$res = wp_remote_get( $this->get_gh_api() );
+		if ( 200 !== wp_remote_retrieve_response_code( $res ) ) {
+			return new WP_Error( wp_remote_retrieve_body( $res ) );
+		}
 		$body = wp_remote_retrieve_body( $res );
 		return json_decode( $body );
 	}
 
-	private function get_gh_api()
+	private function get_gh_api( $endpoint = 'releases' )
 	{
-		return sprintf(
-			'https://api.github.com/repos/%s/%s/releases/latest',
+		return esc_url_raw( sprintf(
+			'https://api.github.com/repos/%1$s/%2$s/%3$s/latest',
 			$this->gh_user,
-			$this->gh_repo
-		);
+			$this->gh_repo,
+			$endpoint
+		), 'https' );
 	}
 }
--- tests/test-gh-auto-updater.php
+++ tests/test-gh-auto-updater.php
@@ -21,7 +21,8 @@ class GH_Auto_Update_Test extends WP_UnitTestCase
 	{
 		$gh_user = 'miya0001';
 		$gh_repo = 'gh-auto-updater-example';
-		$updater = new GH_Auto_Update( __FILE__, $gh_user, $gh_repo );
+		$plugin_slug = 'hello/hello.php';
+		$updater = new GH_Auto_Update( $plugin_slug, $gh_user, $gh_repo );
 
 		// For a private function.
 		$reflection = new \ReflectionClass( $updater );
@@ -41,7 +42,8 @@ class GH_Auto_Update_Test extends WP_UnitTestCase
 	{
 		$gh_user = 'miya0001';
 		$gh_repo = 'gh-auto-updater-example';
-		$updater = new GH_Auto_Update( __FILE__, $gh_user, $gh_repo );
+		$plugin_slug = 'hello/hello.php';
+		$updater = new GH_Auto_Update( $plugin_slug, $gh_user, $gh_repo );
 
 		// For a private function.
 		$reflection = new \ReflectionClass( $updater );
@@ -52,4 +54,83 @@ class GH_Auto_Update_Test extends WP_UnitTestCase
 		$this->assertTrue( 0 === strpos( $res->url, $expect ) );
 		$this->assertSame( 1, count( $res->assets ) );
 	}
+
+	/**
+	 * get_dowload_url()
+	 */
+	function test_get_dowload_url()
+	{
+		$gh_user = 'miya0001';
+		$gh_repo = 'gh-auto-updater-example';
+		$plugin_slug = 'hello/hello.php';
+		$updater = new GH_Auto_Update( $plugin_slug, $gh_user, $gh_repo );
+
+		$mock = dirname( __FILE__ ) . '/remote-version.json';
+		$remote_version = json_decode( file_get_contents( $mock ) );
+
+		$reflection = new \ReflectionClass( $updater );
+		$method = $reflection->getMethod( 'get_download_url' );
+		$method->setAccessible( true );
+		$res = $method->invoke( $updater, $remote_version );
+
+		$this->assertSame(
+			substr( $res, 0 - strlen( 'gh-auto-updater-example.zip' ) ),
+			'gh-auto-updater-example.zip'
+		);
+	}
+
+	/**
+	 * get_newer_version()
+	 */
+	function test_get_newer_version()
+	{
+		$gh_user = 'miya0001';
+		$gh_repo = 'gh-auto-updater-example';
+		$plugin_slug = 'hello/hello.php';
+		$updater = new GH_Auto_Update( $plugin_slug, $gh_user, $gh_repo );
+
+		$transient = new \stdClass();
+		$transient->response = array();
+
+		$mock = dirname( __FILE__ ) . '/remote-version.json';
+		$remote_version = json_decode( file_get_contents( $mock ) );
+
+		$reflection = new \ReflectionClass( $updater );
+		$method = $reflection->getMethod( 'get_newer_version' );
+		$method->setAccessible( true );
+
+		$current_version = array( 'Version' => '0.1.0' );
+		$res = $method->invoke( $updater, $transient, $current_version, $remote_version );
+		$this->assertTrue( !! count( $res->response ) );
+
+		$current_version = array( 'Version' => '4.1.0' );
+		$res = $method->invoke( $updater, $transient, $current_version, $remote_version );
+		$this->assertTrue( !! count( $res->response ) );
+	}
+
+	/**
+	 * get_newer_version()
+	 */
+	function test_get_newer_version_with_new_version()
+	{
+		$gh_user = 'miya0001';
+		$gh_repo = 'gh-auto-updater-example';
+		$plugin_slug = 'hello/hello.php';
+		$updater = new GH_Auto_Update( $plugin_slug, $gh_user, $gh_repo );
+
+		$transient = new \stdClass();
+		$transient->response = array();
+
+		$mock = dirname( __FILE__ ) . '/remote-version.json';
+		$remote_version = json_decode( file_get_contents( $mock ) );
+
+		$reflection = new \ReflectionClass( $updater );
+		$method = $reflection->getMethod( 'get_newer_version' );
+		$method->setAccessible( true );
+
+		$current_version = array( 'Version' => '4.1.0' );
+		$res = $method->invoke( $updater, $transient, $current_version, $remote_version );
+		$this->assertTrue( ! count( $res->response ) );
+	}
+
 }
